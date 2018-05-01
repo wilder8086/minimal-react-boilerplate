ec0a505552fd9a62b9e85c3620612eae78e41056
Wilder Delgado <willy8086@gmail.com>
2018-05-01 18:39:53 -0300

    Subo la estructura del proyecto react

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+const title = 'My Minimal React Webpack Babel Setup' + '  -   '+ 'https://www.robinwieruch.de/minimal-react-webpack-babel-setup/';
+
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
+
+
+
+
+console.log('My Minimal React Webpack Babel Setup');
